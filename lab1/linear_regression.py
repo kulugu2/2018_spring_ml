18deaeeebaad0a2e8a063ce50233ee9733ee875a
Jeff Chou <kulugu2@gmail.com>
2018-03-09 17:09:57 +0800

    inverse

--- lab1/linear_regression.py
+++ lab1/linear_regression.py
@@ -9,10 +9,10 @@ def M_add(A, B):
     return C
 
 def M_mul(A, B):
-    C = [ [0 for i in range(len(A))] for j in range(len(A))]
+    C = [ [0 for i in range(len(B[0]))] for j in range(len(A))]
     for i in range(len(A)):
-        for j in range(len(A)):
-            for k in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(A[0])):
                 C[i][j] += A[i][k] * B[k][j]
     return C
 
@@ -43,6 +43,40 @@ def  M_mul_scalar(M, s):
         M[i] = map(lambda x: x*s, M[i])
     return M
 
+def inverse(M):
+    print(M[:])
+    L, U = LU_decompose(M)
+    print(L[:])
+    n = len(L)
+
+    """ L inverse  """
+    LI = [[0 for i in range(n)] for j in range(n)]
+    for i in range(n):
+        LI[i][i] = 1/float(L[i][i])
+        for j in range(i+1, n):
+            for k in range(j):
+                LI[j][i] -= L[j][k]*LI[k][i]
+            LI[j][i] = LI[j][i]/float(L[j][j])
+    print(LI[:])
+    
+    """ U inverse """
+    print("U")
+    print(U[:])
+    UI = [[0 for i in range(n)] for j in range(n)]
+    for i in range(n-1, -1, -1):
+        UI[i][i] = 1/float(U[i][i])
+        for j in range(i-1, -1, -1):
+            for k in range(n-1, j, -1):
+                UI[j][i] -= U[j][k]*UI[k][i]
+            UI[j][i] = UI[j][i]/float(U[j][j])
+
+    print(UI[:])
+
+    MI = M_mul(UI, LI)
+    return MI
+
+def transpose(M):
+    return [[M[i][j] for i in range(len(M))] for j in range(len(M[0]))]
 if __name__ == '__main__':
     A = [[1,2,3], [4,5,6] , [7,8,9]]
     B = eye(3)
@@ -50,9 +84,11 @@ if __name__ == '__main__':
     D = [[24,3,45], [-2,7,-10], [8,21,24]]
     E = [[3,-0.1,-0.2], [0.1,7,-0.3],[0.3, -0.2,10]]
     M = M_add(A,B)
+    I = [[3,18,24], [-2,-7,-36],[1,9,-2]]
+    G = [[1,2,3],[4,5,6]]
+    H = [[-2,35],[32,4],[0.5,1.25]]
+    K = transpose(H)
     
-    K = M_mul_scalar(A,3)
-
 #    L,U = LU_decompose(C)
 #    print(range(3,3))
     for i in K :
