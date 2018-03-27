fb9ce626e7a06e0bf43145fefb5b444e340b2a61
Jeff Chou <kulugu2@gmail.com>
2018-03-27 21:57:37 +0800

    finish v1

--- lab2/naive_bayes.py
+++ lab2/naive_bayes.py
@@ -1,10 +1,10 @@
 import sys
 import math
 
-bin_num = 2
+bin_num = 8
 def discrete(image_fp, label_fp, np, n):
     
-    table = [[[0 for i in range(10)] for j in range(bin_num)] for k in range(np)]  #table[784][32][10]
+    table = [[[1 for i in range(10)] for j in range(bin_num)] for k in range(np)]  #table[784][32][10]
     num_per_digit = [0 for i in range(10)]
     for i in range(n):
         label = int.from_bytes(label_fp.read(1), byteorder='big')
@@ -18,7 +18,7 @@ def discrete(image_fp, label_fp, np, n):
 
 def discrete_predict(table, num_per_digit, np, n, fp):
     
-    posterior = [1.0 for i in range(10)]
+    posterior = [0.0 for i in range(10)]
 
     image = [None]*np
     for i in range(np):
@@ -32,8 +32,8 @@ def discrete_predict(table, num_per_digit, np, n, fp):
                 pj = 1 / float(num_per_digit[i])
             else :
                 pj = table[j][value][i] / float(num_per_digit[i])
-            posterior[i] *= pj
-        posterior[i] *= num_per_digit[i] / float(n)       
+            posterior[i] += math.log(pj)
+        posterior[i] += math.log(num_per_digit[i] / float(n))       
     
     return posterior
 
@@ -66,9 +66,12 @@ def continuous_predict(table, num_per_digit, np, n, fp):
             var = (table[j][i][1] / float(num_per_digit[i])) - mean**2
             if var == 0 :
                 var = 0.0001
-            pj = math.exp(-(value-mean)**2/(2*var)) / math.sqrt(2*math.pi*var)
-            posterior[i] *= pj
-        posterior[i] *= num_per_digit[i] / float(n)
+            value = (value-mean) / float(var**0.5)
+            pj = math.exp((-value**2/(2.0)) / math.sqrt(2.0*math.pi))
+            if pj<0.0001:
+                pj = 0.0001
+            posterior[i] += math.log(pj)
+        posterior[i] += math.log(num_per_digit[i] / float(n))
 
     return posterior
             
@@ -79,7 +82,7 @@ if __name__ == '__main__':
     label_fp = open(sys.argv[2], 'rb')
     test_im = open(sys.argv[3], 'rb')
     test_lb = open(sys.argv[4], 'rb')
-
+    mode = int(sys.argv[5])
 
     magic_number = int.from_bytes(image_fp.read(4), byteorder='big')
     n = int.from_bytes(image_fp.read(4), byteorder='big')
@@ -88,25 +91,28 @@ if __name__ == '__main__':
     
     num_of_pixels = image_row * image_col
     label_fp.seek(8)
-    #t, num_per_digit = discrete(image_fp, label_fp, num_of_pixels, n)
-    t, num_per_digit = continuous(image_fp, label_fp, num_of_pixels, 10000)
-    for i in range(num_of_pixels):
-        print(t[i])
+    if mode == 0:
+        t, num_per_digit = discrete(image_fp, label_fp, num_of_pixels, n)
+    else :
+        t, num_per_digit = continuous(image_fp, label_fp, num_of_pixels, n)
     test_im.read(4)
     test_n = int.from_bytes(test_im.read(4), byteorder='big')
     test_im.read(8)
     test_lb.read(8)
     match = 0
-    for i in range(10):
-        #p = discrete_predict(t, num_per_digit, num_of_pixels, n, test_im)
-        p = continuous_predict(t, num_per_digit, num_of_pixels, 10000, test_im)
+    
+    for i in range(test_n):
+        if mode == 0:
+            p = discrete_predict(t, num_per_digit, num_of_pixels, n, test_im)
+        else :
+            p = continuous_predict(t, num_per_digit, num_of_pixels, n, test_im)
         predict = p.index(max(p))
         real_y = int.from_bytes(test_lb.read(1), byteorder='big')
-        #print(predict, real_y, p)
+        #print(predict, real_yi, p)
         if predict == real_y :
             match += 1
     #print(p)
-    print(match / float(10))
+    print(match / float(test_n))
     
     #print(t)
 
---
+++ lab2/online_learning.py
@@ -0,0 +1,38 @@
+import sys
+
+def factorial(n):
+    result = 1
+    if n == 0:
+        return 1
+    for i in range(1, n+1):
+        result *= i
+    
+    return result
+
+
+if __name__ == '__main__':
+    fp = open(sys.argv[1], 'r')
+    initial_a = int(sys.argv[2])
+    initial_b = int(sys.argv[3])
+    
+    a = initial_a
+    b = initial_b
+    for line in iter(fp):
+        N = 0
+        m = 0
+        for c in line:
+            if c == '\n':
+                break
+            if int(c) == 1:
+                m += 1
+                N += 1
+            else:
+                N += 1
+        likelihood = m/float(N)
+        print('likelihood = %f' %(likelihood))
+        print('prior: a=%d, b=%d' %(a,b))
+        print('posterior: a=%d, b=%d' %(a+m, N-m+b))
+        a = a+m
+        b = b+N-m
+        print('')    
+            
